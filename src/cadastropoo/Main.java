805a6a3ed9e8691154138e406a07ce3b82c0726c
CRFlam <jjsmithcrf@gmail.com>
2024-03-27 11:11:35 -0300

    Iniciando projeto

---
+++ src/cadastropoo/Main.java
@@ -0,0 +1,82 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cadastropoo;
+
+/**
+ *
+ * @author Smith
+ */
+
+import java.io.IOException;
+import model.PessoaFisica;
+import model.PessoaFisicaRepo;
+import model.PessoaJuridica;
+import model.PessoaJuridicaRepo;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        PessoaFisicaRepo repo1 = new PessoaFisicaRepo();
+
+        PessoaFisica pf1 = new PessoaFisica(1, "Jeferson Smith", "123.456.789-00", 18);
+        PessoaFisica pf2 = new PessoaFisica(2, "Daniela Smith", "111.111.111-11", 18);
+
+        repo1.inserir(pf1);
+        repo1.inserir(pf2);
+
+        String nomeArquivo = "pessoasFisicas.txt";
+        try {
+            repo1.persistir(nomeArquivo);
+        } catch (IOException e) {
+            System.err.println("Erro: " + e);
+        }
+
+        PessoaFisicaRepo repo2 = new PessoaFisicaRepo();
+
+        try {
+            repo2.recuperar(nomeArquivo);
+            System.out.println("Dados de Pessoas Fisicas Armazenados.");
+        } catch (IOException | ClassNotFoundException e) {
+            System.err.println("Erro: " + e);
+        }
+
+        System.out.println("Dados de Pessoa Fisica Recuperados.");
+        for (PessoaFisica pf : repo2.obterTodos()) {
+            pf.exibir();
+        }
+
+        PessoaJuridicaRepo repo3 = new PessoaJuridicaRepo();
+
+        PessoaJuridica pj1 = new PessoaJuridica(3, "Empresa 1", "11.111.111/0001-01");
+        PessoaJuridica pj2 = new PessoaJuridica(4, "Empresa 2", "11.111.111/0001-02");
+
+        repo3.inserir(pj1);
+        repo3.inserir(pj2);
+
+        String nomeArquivoPJ = "pessoasJuridicas.txt";
+        try {
+            repo3.persistir(nomeArquivoPJ);
+            System.out.println("\nDados de Pessoas Juridicas Armazenados.");
+        } catch (IOException e) {
+            System.err.println("Erro Encontrado: " + e);
+        }
+
+        PessoaJuridicaRepo repo4 = new PessoaJuridicaRepo();
+
+        String nomeArquivoPJ2 = "pessoasJuridicas.txt"; 
+        try {
+            repo4.recuperar(nomeArquivoPJ2);
+        } catch (IOException | ClassNotFoundException e) {
+            System.err.println("Erro Encontrado: " + e);
+        }
+
+        System.out.println("Dados de Pessoas Juridicas Recuperados.");
+        for (PessoaJuridica pj : repo4.obterTodos()) {
+            pj.exibir();
+        }
+
+    }
+}
---
+++ src/model/PessoaFisicaRepo.java
@@ -0,0 +1,60 @@
+package model;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PessoaFisicaRepo {
+
+    private List<PessoaFisica> pessoasFisicas;
+
+    public PessoaFisicaRepo() {
+        this.pessoasFisicas = new ArrayList<>();
+    }
+
+    public void inserir(PessoaFisica pessoaFisica) {
+        pessoasFisicas.add(pessoaFisica);
+    }
+
+    public void alterar(PessoaFisica pessoaFisica) {
+        for (int i = 0; i < pessoasFisicas.size(); i++) {
+            if (pessoasFisicas.get(i).getId() == pessoaFisica.getId()) {
+                pessoasFisicas.set(i, pessoaFisica);
+                return;
+            }
+        }
+    }
+
+    public void excluir(int id) {
+        pessoasFisicas.removeIf(pessoaFisica -> pessoaFisica.getId() == id);
+    }
+
+    public PessoaFisica obter(int id) {
+        for (PessoaFisica pf : pessoasFisicas) {
+            if (pf.getId() == id) {
+                return pf;
+            }
+        }
+        return null;
+    }
+
+    public List<PessoaFisica> obterTodos() {
+        return new ArrayList<>(pessoasFisicas);
+    }
+
+    public void persistir(String nomeArquivo) throws IOException {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(nomeArquivo))) {
+            oos.writeObject(pessoasFisicas);
+        }
+    }
+
+    public void recuperar(String nomeArquivo) throws IOException, ClassNotFoundException {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(nomeArquivo))) {
+            pessoasFisicas = (ArrayList<PessoaFisica>) ois.readObject();
+        }
+    }
+}
